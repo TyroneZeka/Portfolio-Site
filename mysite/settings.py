61b685aa6ceb5dbb65fcfd698c9c78e92f20f628
Tyrone <zekah54@gmail.com>
2021-12-01 14:27:03 +0800

    debug true

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ from decouple import config
 
 SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
 
-DEBUG = False
+DEBUG = True
 
 # EMAIL SETUP WITH GMAIL
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
